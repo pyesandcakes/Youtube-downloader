5b18046a16d30ee812f3c45d807142bdd765f5fb
pyesandcakes <bsimphonycool@gmail.com>
2023-03-24 22:10:37 +0100

    add

--- downloader.py
+++ downloader.py
@@ -9,7 +9,7 @@ video = YouTube(link, on_progress_callback=on_progress)
 descr = video.description
 info = descr[:75] + (descr[75:] and '..')
 yd_vid = video.streams.get_highest_resolution()
-#yd_aud = video.streams.get_audio_only()
+yd_aud = video.streams.get_audio_only()
 print(video.title)
 print(info)
 
@@ -22,18 +22,18 @@ if user_input == 'Yes':
 
 quit()
 
-#user_input = ''
+user_input_v = ''
 
-#while True:
-#    user_input = input(
-#        'Pick one: 1) Video | 2) Audio ')
+while True:
+    user_input_v = input(
+        'Pick one: 1) Video | 2) Audio ')
 
-#    if user_input == '1' or "Video" or "video":
-#        print('Downloading video...')
- #       yd_vid.download("C:/Users/Bruno/Documents/Youtube downloader/Video")
- #       yd_vid.on_progress
- #       break
- #   elif user_input == '2' or "Audio" or "audio":
- #       print('Downloading audio...')
- #       yd_aud.download("C:/Users/Bruno/Documents/Youtube downloader/Audio")
-  #      break
+    if user_input_v == '1' or "Video" or "video":
+        print('Downloading video...')
+       yd_vid.download("C:/Users/Bruno/Documents/Youtube downloader/Video")
+       yd_vid.on_progress
+       break
+   elif user_input_v == '2' or "Audio" or "audio":
+       print('Downloading audio...')
+       yd_aud.download("C:/Users/Bruno/Documents/Youtube downloader/Audio")
+      break
