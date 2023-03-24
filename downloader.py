ce688787ab11459c655e611ed877361a28482573
pyesandcakes <bsimphonycool@gmail.com>
2023-03-25 00:35:20 +0100

    Finished. Only need channel function.

--- downloader.py
+++ downloader.py
@@ -5,6 +5,8 @@ from pytube import Channel
 from pytube import Playlist
 
 dwpth_vid=os.getcwd()+"\Video"
+dwpth_vid_pl=os.getcwd()+"\Video\Playlists"
+dwpth_aud_pl=os.getcwd()+"\Audio\Playlists"
 dwpth_aud=os.getcwd()+"\Audio"
 
 one_input=""
@@ -51,13 +53,36 @@ if one_input == "1":
 if one_input == "3":
         pl_link=input("Insert playlist URL ")
         print("Searching...")
-        p = Playlist(pl_link)
-        for video in p.videos:
-                video.register_on_progress_callback(on_progress)
-                pl_str=video.streams.get_highest_resolution()
-                print(video.title)
-                print()
-                pl_str.download(dwpth_vid)
-                print()
-                pl_str.on_progress
-                print()
+        pl_str = Playlist(pl_link)
+        print("Found",pl_str.title)
+        pth=pl_str.title
+        pth=pth.replace(" ", "")
+        print(pl_str.length,"videos found.")
+        user_input = ''
+        user_input = input('Pick one: 1) Video | 2) Audio ')
+        if user_input == '1':
+                print('Downloading videos...')
+                dwpth_vid_pl +="\\"+pth
+                for video in pl_str.videos:
+                        video.register_on_progress_callback(on_progress)
+                        pl_str=video.streams.get_highest_resolution()
+                        print(video.title)
+                        print()
+                        pl_str.download(dwpth_vid_pl)
+                        print()
+                        pl_str.on_progress
+                        print()
+                print("\n Download finished. \n Find files in", dwpth_vid_pl)
+        elif user_input == '2':
+                print('Downloading audio...')
+                dwpth_aud_pl +="\\"+pth
+                for video in pl_str.videos:
+                        video.register_on_progress_callback(on_progress)
+                        pl_str=video.streams.get_audio_only()
+                        print(video.title)
+                        print()
+                        pl_str.download(dwpth_aud_pl)
+                        print()
+                        pl_str.on_progress
+                        print()
+                print("\n Download finished. \n Find files in", dwpth_aud_pl)
